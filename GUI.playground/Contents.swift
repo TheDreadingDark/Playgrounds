dc56cda2c524c3a4cba58bbac7298eb080df27f6
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-04 13:53:35 -0600

    Created Star Wahs button.

--- GUI.playground/Contents.swift
+++ GUI.playground/Contents.swift
@@ -7,3 +7,6 @@ var topic = "Working with GUI :D"
 var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
 myFirstButton.setTitle("Words", for: .normal)
 myFirstButton.backgroundColor = .green
+var mySecondButton = UIButton(frame: CGRect(x:30, y:60, width: 200, height: 120))
+mySecondButton.setTitle("Star Wahs", for: .normal)
+mySecondButton.backgroundColor = .red
